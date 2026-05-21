7a84c15f3e335bdf9ea190e15095fb09997b8c0e
nastoikazhenshen <ivaikinadaria15@gmail.com>
2026-05-21 18:21:40 +0500

    feat: add auth dto and jwt dependencies

---
+++ src/main/java/kz/gamematch/dto/auth/RegisterRequestDto.java
@@ -0,0 +1,24 @@
+package kz.gamematch.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequestDto {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 100)
+    private String password;
+
+    @NotBlank
+    @Size(min = 3, max = 80)
+    private String nickname;
+}
