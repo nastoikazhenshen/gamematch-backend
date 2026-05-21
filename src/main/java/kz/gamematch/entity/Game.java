0c743a085db91ddffb8b5db52768e94fcfeec308
nastoikazhenshen <ivaikinadaria15@gmail.com>
2026-05-21 17:23:20 +0500

    feat: add profile and game entities

--- src/main/java/kz/gamematch/entity/Game.java
+++ src/main/java/kz/gamematch/entity/Game.java
@@ -1,4 +1,19 @@
 package kz.gamematch.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "games")
+@Getter
+@Setter
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 80)
+    private String name;
 }
